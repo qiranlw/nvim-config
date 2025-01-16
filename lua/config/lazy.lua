59aed988f1289910bf2934460efaa6c764c3a00d
qiranlw <qiranlw@gmail.com>
2025-01-17 00:44:17 +0800

    add snacks.nvim config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -62,6 +62,22 @@ require("lazy").setup({
                 },
             },
         },
+        {
+            "folke/snacks.nvim",
+            priority = 1000,
+            lazy = false,
+            opts = {
+                bigfile = { enabled = true },
+                dashboard = { enabled = true },
+                indent = { enabled = true },
+                input = { enabled = true },
+                notifier = { enabled = true },
+                quickfile = { enabled = true },
+                scroll = { enabled = true },
+                statuscolumn = { enabled = true },
+                words = { enabled = true },
+            },
+        },
     },
     checker = { enabled = true },
 })
